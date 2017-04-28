74d5bee0cbd3ded3c51444c850280d3441b17ae0
Marek <marek.b@cstechnologies.cz>
2017-04-28 12:20:38 +0200

    README.md

--- .vscode-test/es5-project/test/utility.test.js
+++ .vscode-test/es5-project/test/utility.test.js
@@ -2,84 +2,22 @@ const Mocha = require('mocha');
 const expect = require('chai').expect;
 const utils = require('../src/utility');
 
-//describe('commented out', function () {
-//    it('test', function () {
-//    });
-//});
-
-/*    
-    describe('commented out', function () {
-        it('test', function () {
-        });
-    });
-*/
-
-/* comment */ describe('comments #1', function () {
-    it('test', function () {
-    });
-});
-
-describe(/* comment */ 'comments #2', function () {
-    it('test', function () {
-    });
-});
-
-describe('comments #3' /* comment */, function () {
-    it('test', function () {
-    });
-});
-
-describe('multi line #1',
-    function () {
-        it('test', function () {
-        });
-    }
-);
-
-describe(
-    'multi line #2',
-    function () {
-        it('test', function () {
-        });
-    }
-);
-
-describe('multi line #3', // comment
-    function () {  // comment
-        it('test', function () {
-        });
-    }
-);
-
-describe( // comment
-    'multi line #4', // comment
-    function () {
-        it('test', function () {
-        });
-    }
-);
-
-describe('same name', function () {
-    it('test #1', function () {
-    });
-});
-
-describe('same name', function () {
-    it('test #2', function () {
-    });
-});
-
-
-
 describe('Utility', function () {
     it('Success', function () {
         expect(utils.add(1, 2)).to.equal(3);
     });
-
-    // nesting is fine ...    
+    
+    it('Fail', function () {
+        expect(utils.add(1, 2)).to.equal(0);
+    });
+ 
     describe('nested', function () {
         it('nested Success', function () {
             expect(utils.add(1, 2)).to.equal(3);
+        });
+        
+        it('nested Fail', function () {
+            expect(utils.add(1, 2)).to.equal(0);
         });
 
         describe('double nested', function () {
@@ -92,13 +30,5 @@ describe('Utility', function () {
             });
 
         });
-
-        it('nested Fail', function () {
-            expect(utils.add(1, 2)).to.equal(0);
-        });
-    });
-
-    it('Fail', function () {
-        expect(utils.add(1, 2)).to.equal(0);
     });
 }); 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vscode-mocha-test-runner
+# Mocha test runner 
 
 Creates code lens before each **describe** and **it** function call in files with these patterns: ***.test.js**, ***.test.jsx**, ***.test.ts** and ***.test.tsx**.
 
@@ -6,9 +6,17 @@ Lense on **it** function shows last test result and can be used to run given tes
 
 Lense on **describe** shows aggregated results from nested **it** and can be used to run all tests in given group.
 
-## Preview
+Tests not writen in es5 (es6, razor, typescript, etc.) needs to be transpiled to es5 so mocha can load them - see example projects in .vscode-test directory.
+
+## Preview:
 
 ![preview](./images/preview.png)
+
+## Examples:
+- [es5 project](.vscode-test/es5-project).
+- [es6 project](.vscode-test/es6-project)
+- [typescript project](.vscode-test/ts-project)
+- [enzyme project](.vscode-test/enzyme-project)
 
 
 ## Known issues:
